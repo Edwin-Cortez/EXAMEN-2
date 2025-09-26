c6a4c9df919773f868cf38964f0e7182ae244502
Edwin-Cortez <ecortezt@miumg.edu.gt>
2025-09-25 23:12:04 -0600

    Add files via upload

---
+++ AttackMissedException.java
@@ -0,0 +1,12 @@
+package org.example.exceptions;
+
+public class AttackMissedException extends Exception {
+    public AttackMissedException(String mensaje) {
+        super(mensaje);
+    }
+
+    public AttackMissedException() {
+        super("El ataque fallo");
+    }
+}
+
---
+++ InvalidChoiceException.java
@@ -0,0 +1,12 @@
+package org.example.exceptions;
+
+public class InvalidChoiceException extends Exception {
+    public InvalidChoiceException(String mensaje) {
+        super(mensaje);
+    }
+
+    public InvalidChoiceException() {
+        super("Opcion invalida");
+    }
+}
+
